62af98c674ac14fa1659d932b32c6e9b62f49800
G4mingJon4s <Jthelker@gmail.com>
2023-04-04 21:12:55 +0200

    added splitting network ram

--- src/stanek/stanek.ts
+++ src/stanek/stanek.ts
@@ -23,6 +23,22 @@ export async function main(ns: NS) {
 	}
 }
 
+export const STANEKRAMPERCENTAGES: Record<string, number> = {
+	"n00dles": 1,
+	"foodnstuff": 1,
+	"sigma-cosmetics": 1,
+	"joesguns": 1,
+	"hong-fang-tea": 1,
+	"harakiri-sushi": 1,
+	"nectar-net": 1,
+};
+
+export const STANEKBASEPERCENTAGE = 0.3;
+
+export function getServerStanekRamPercentage(server: string) {
+	return Object.entries(STANEKRAMPERCENTAGES).find(o => o[0] === server)?.[1] ?? STANEKBASEPERCENTAGE;
+}
+
 export function display(ns: NS) {
 	const allFrags = ns.stanek.activeFragments().filter(frag => frag.id < 100);
 
@@ -38,11 +54,11 @@ export function display(ns: NS) {
 }
 
 export function launchCharge(ns: NS, allFrags: ActiveFragment[], invalidHosts: string[] = []) {
-	const hosts = getHosts(ns, ns.getScriptRam(CHARGEFILE) * allFrags.length, invalidHosts);
+	const hosts = getHosts(ns, ns.getScriptRam(CHARGEFILE) * allFrags.length, invalidHosts, server => getServerStanekRamPercentage(server.hostname));
 
 	hosts.forEach(host => ns.scp(CHARGEFILE, host, "home"));
 
-	const mapped = mapHosts(ns, hosts, ns.getScriptRam(CHARGEFILE) * allFrags.length);
+	const mapped = mapHosts(ns, hosts, ns.getScriptRam(CHARGEFILE) * allFrags.length, undefined, undefined, server => getServerStanekRamPercentage(server.hostname));
 
 	const pids = mapped.flatMap(host => allFrags.map(frag => ns.exec(CHARGEFILE, host.server, host.threads, frag.x, frag.y)));
 
