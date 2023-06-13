2c206c21db7b0382483d8a21116f86aacde73032
G4mingJon4s <Jthelker@gmail.com>
2023-06-13 16:15:54 +0200

    first tests linking ns and react

--- src/components/test.tsx
+++ src/components/test.tsx
@@ -1,10 +1,43 @@
-export default function Test() {
-	const [count, setCount] = React.useState(0);
+import type { get, set, subscribe } from "/tests/cct";
+
+const button: React.CSSProperties = {
+	border: "2px solid white",
+	background: "#ff6060",
+	margin: "0.5rem",
+	padding: "0.2rem",
+	cursor: "pointer",
+	fontSize: "18px",
+	justifyContent: "center"
+};
+
+const header: React.CSSProperties = {
+	color: "white",
+	justifyContent: "center",
+	alignItems: "center"
+};
+
+const container: React.CSSProperties = {
+	height: "300px",
+	width: "500px",
+	display: "flex",
+	flexDirection: "column"
+};
+
+interface Props {
+	get: get<number>;
+	set: set<number>;
+	subscribe: subscribe<number>;
+}
+
+export default function Test({ get, set, subscribe }: Props) {
+	const [_, rerender] = React.useState(false);
+
+	React.useEffect(() => subscribe(() => rerender(a => !a)), []);
 
 	return (
-		<div>
-			<h1>Hello there!</h1>
-			<button onClick={() => setCount(c => c + 1)}>Count: {count}</button>
+		<div style={container}>
+			<h1 style={header}>Hello there!</h1>
+			<button onClick={() => set(a => a + 1)} style={button}>Count: {get()}</button>
 		</div>
 	);
 }
---
+++ src/tests/cct.tsx
@@ -0,0 +1,44 @@
+import type { NS } from "@ns";
+import Test from "components/test";
+
+export async function main(ns: NS) {
+	ns.disableLog("ALL"); ns.clearLog(); ns.tail(); ns.atExit(() => ns.closeTail());
+
+	const [value, setValue, subscribe] = nsState(0);
+
+	// @ts-expect-error Property 'printRaw' does not exist on type 'NS'.
+	ns.printRaw(<Test get={value} set={setValue} subscribe={subscribe}/>);
+
+	while(true) {
+		await ns.asleep(1000);
+		ns.tprint(value());
+		if (Math.random() > 0.5) setValue(value() + 1);
+	}
+}
+
+export type get<T> = () => T;
+export type set<T> = (value: T | ((value: T) => T)) => void;
+export type action<T> = (value: T) => void;
+export type subscribe<T> = (action: action<T>) => () => undefined;
+
+export function nsState<T>(value: T): [get<T>, set<T>, subscribe<T>] {
+	let state = value;
+
+	const actions = new Map<string, action<T>>();
+
+	const subscribe: subscribe<T> = action => {
+		const id = crypto.randomUUID();
+		actions.set(id, action);
+
+		return () => void actions.delete(id);
+	};
+
+	const get: get<T> = () => state;
+	const set: set<T> = value => {
+		state = value instanceof Function ? value(state) : value;
+
+		actions.forEach(a => a(state));
+	};
+
+	return [get, set, subscribe];
+}
